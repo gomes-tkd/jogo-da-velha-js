35ef1c4efa3953120af363f85d37195dc00e5ae0
José Gomes <106411184+gomes-tkd@users.noreply.github.com>
2022-12-29 17:15:42 -0300

    contador empate

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const tabuleiro = document.querySelector(".tabuleiro");
+const tabuleiroLinhas = document.querySelectorAll("tr");
+const tabuleiroCelulas = document.querySelectorAll("td");
 const resultadoX = document.querySelector(".resultado-x");
 const resultadoO = document.querySelector(".resultado-o");
 const resultadoEmpate = document.querySelector(".resultado-empate");
@@ -13,11 +15,28 @@ let totalPartidasContador = 0;
 
 
 function handleTurno(x) {
-    if (turnoContador % 2 !== 0) {
-        x.innerHTML = "X";
+    if (turnoContador <= 8) {
+
+        if (turnoContador % 2 !== 0) {
+            x.innerHTML = "X";
+        } else {
+            x.innerHTML = "O";
+        }
     } else {
-        x.innerHTML = "O";
+        alert("VELHA, deu empate! Limite máximo de movimentos alcançado!");
+        empateContador++;
+        totalPartidasContador++;
+
+        resultadoEmpate.innerHTML = empateContador.toString();
+        totalPartidas.innerHTML = totalPartidasContador.toString();
+        turno.innerHTML = turnoContador.toString();
+
+        tabuleiroCelulas.forEach(e => e.innerHTML = "");
+        turnoContador = 0;
+
     }
     turnoContador++;
+
     turno.innerHTML = turnoContador.toString();
+
 }
