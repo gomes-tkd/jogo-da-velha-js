e29b996dd8022ef039ae2abecd1d7ab570f20c65
José Gomes <106411184+gomes-tkd@users.noreply.github.com>
2022-12-29 23:07:14 -0300

    condicoes de vitoria

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-const tabuleiro = document.querySelector(".tabuleiro");
-const tabuleiroLinhas = document.querySelectorAll("tr");
+const jogadorAtual = document.querySelector(".jogador-atual");
 const tabuleiroCelulas = document.querySelectorAll("td");
 const resultadoX = document.querySelector(".resultado-x");
 const resultadoO = document.querySelector(".resultado-o");
@@ -15,28 +14,122 @@ let totalPartidasContador = 0;
 
 
 function handleTurno(x) {
-    if (turnoContador <= 8) {
+    const tabuleiro = document.querySelector(".tabuleiro").rows;
 
+    if (turnoContador <= 8) {
         if (turnoContador % 2 !== 0) {
             x.innerHTML = "X";
         } else {
             x.innerHTML = "O";
         }
+
     } else {
         alert("VELHA, deu empate! Limite máximo de movimentos alcançado!");
         empateContador++;
-        totalPartidasContador++;
 
         resultadoEmpate.innerHTML = empateContador.toString();
-        totalPartidas.innerHTML = totalPartidasContador.toString();
         turno.innerHTML = turnoContador.toString();
 
-        tabuleiroCelulas.forEach(e => e.innerHTML = "");
-        turnoContador = 0;
-
+        handleClearBoard();
     }
+
+    handleCondicaoVitoria(tabuleiro);
     turnoContador++;
 
     turno.innerHTML = turnoContador.toString();
 
+}
+
+function handleCondicaoVitoria(tabuleiro) {
+    if (tabuleiro[0].cells[0].innerHTML === "X" && tabuleiro[0].cells[1].innerHTML === "X" && tabuleiro[0].cells[2].innerHTML === "X" ) {
+        vitoriaXContador++;
+        resultadoX.innerHTML = vitoriaXContador.toString();
+        alert("Jogador X venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[1].cells[0].innerHTML === "X" && tabuleiro[1].cells[1].innerHTML === "X" && tabuleiro[1].cells[2].innerHTML === "X" ) {
+        vitoriaXContador++;
+        resultadoX.innerHTML = vitoriaXContador.toString();
+        alert("Jogador X venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[2].cells[0].innerHTML === "X" && tabuleiro[2].cells[1].innerHTML === "X" && tabuleiro[2].cells[2].innerHTML === "X" ) {
+        vitoriaXContador++;
+        resultadoX.innerHTML = vitoriaXContador.toString();
+        alert("Jogador X venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[0].cells[0].innerHTML === "X" && tabuleiro[1].cells[0].innerHTML === "X" && tabuleiro[2].cells[0].innerHTML === "X" ) {
+        vitoriaXContador++;
+        resultadoX.innerHTML = vitoriaXContador.toString();
+        alert("Jogador X venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[0].cells[1].innerHTML === "X" && tabuleiro[1].cells[1].innerHTML === "X" && tabuleiro[2].cells[1].innerHTML === "X" ) {
+        vitoriaXContador++;
+        resultadoX.innerHTML = vitoriaXContador.toString();
+        alert("Jogador X venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[0].cells[2].innerHTML === "X" && tabuleiro[1].cells[2].innerHTML === "X" && tabuleiro[2].cells[2].innerHTML === "X" ) {
+        vitoriaXContador++;
+        resultadoX.innerHTML = vitoriaXContador.toString();
+        alert("Jogador X venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[0].cells[0].innerHTML === "X" && tabuleiro[1].cells[1].innerHTML === "X" && tabuleiro[2].cells[2].innerHTML === "X" ) {
+        vitoriaXContador++;
+        resultadoX.innerHTML = vitoriaXContador.toString();
+        alert("Jogador X venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[0].cells[2].innerHTML === "X" && tabuleiro[1].cells[1].innerHTML === "X" && tabuleiro[2].cells[0].innerHTML === "X" ) {
+        vitoriaXContador++;
+        resultadoX.innerHTML = vitoriaXContador.toString();
+        alert("Jogador X venceu!");
+        handleClearBoard();
+    }
+
+    else if (tabuleiro[0].cells[0].innerHTML === "O" && tabuleiro[0].cells[1].innerHTML === "O" && tabuleiro[0].cells[2].innerHTML === "O" ) {
+        vitoriaOContador++;
+        resultadoO.innerHTML = vitoriaOContador.toString();
+        alert("Jogador O venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[1].cells[0].innerHTML === "O" && tabuleiro[1].cells[1].innerHTML === "O" && tabuleiro[1].cells[2].innerHTML === "O" ) {
+        vitoriaOContador++;
+        resultadoO.innerHTML = vitoriaOContador.toString();
+        alert("Jogador O venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[2].cells[0].innerHTML === "O" && tabuleiro[2].cells[1].innerHTML === "O" && tabuleiro[2].cells[2].innerHTML === "O" ) {
+        vitoriaOContador++;
+        resultadoO.innerHTML = vitoriaOContador.toString();
+        alert("Jogador O venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[0].cells[0].innerHTML === "O" && tabuleiro[1].cells[0].innerHTML === "O" && tabuleiro[2].cells[0].innerHTML === "O" ) {
+        vitoriaOContador++;
+        resultadoO.innerHTML = vitoriaOContador.toString();
+        alert("Jogador O venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[0].cells[1].innerHTML === "O" && tabuleiro[1].cells[1].innerHTML === "O" && tabuleiro[2].cells[1].innerHTML === "O" ) {
+        vitoriaOContador++;
+        resultadoO.innerHTML = vitoriaOContador.toString();
+        alert("Jogador O venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[0].cells[2].innerHTML === "O" && tabuleiro[1].cells[2].innerHTML === "O" && tabuleiro[2].cells[2].innerHTML === "O") {
+        vitoriaOContador++;
+        resultadoO.innerHTML = vitoriaOContador.toString();
+        alert("Jogador O venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[0].cells[0].innerHTML === "O" && tabuleiro[1].cells[1].innerHTML === "O" && tabuleiro[2].cells[2].innerHTML === "O" ) {
+        vitoriaOContador++;
+        resultadoO.innerHTML = vitoriaOContador.toString();
+        alert("Jogador O venceu!");
+        handleClearBoard();
+    } else if (tabuleiro[0].cells[2].innerHTML === "O" && tabuleiro[1].cells[1].innerHTML === "O" && tabuleiro[2].cells[0].innerHTML === "O" ) {
+        vitoriaOContador++;
+        resultadoO.innerHTML = vitoriaOContador.toString();
+        alert("Jogador O venceu!");
+        handleClearBoard();
+    }
+}
+
+function handleClearBoard() {
+    tabuleiroCelulas.forEach(e => e.innerHTML = "");
+    turnoContador = 0;
+    turno.innerHTML = turnoContador.toString();
+    totalPartidasContador++;
+    totalPartidas.innerHTML = totalPartidasContador.toString();
 }
